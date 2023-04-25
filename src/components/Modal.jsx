9fe293cb5dbd1a9b34bd050cd145ddf30d1e1789
devjjin <sujink34@naver.com>
2023-04-25 20:21:44 +0900

    Fix: 모달내부를 클릭시 모달창이 닫히는 현상(이벤트 버블링 전파 방지)

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,65 +1,60 @@
 import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
 import { StButton } from './Button';
-import { VscClose }  from 'react-icons/vsc';
+import { VscClose } from 'react-icons/vsc';
 
 function Modal() {
-    const [showModal, setShowModal] = useState(false);
-    const [secShowModal, setSecShowModal] = useState(false);
-    const modalRef = useRef();
-  
-    const openModalHandler = () => setShowModal(!showModal);
-    const openSecModalHandler = () => setSecShowModal(!secShowModal);
-  
-    const handleModalClick = e => {
-      if (modalRef.current === e.target) {
-          return;
-        }
-        setSecShowModal(false);
-    };
-  
-    return (
-      <>
-        <h1>Modal</h1>
-        <ButtonGrop>
-          <StButton primary size='small' onClick={openModalHandler}>
-            open modal
-          </StButton>
-          <StButton negative size='large' onClick={openSecModalHandler}>
-            open modal
-          </StButton>
-        </ButtonGrop>
-  
-        {showModal && (
-          <Wrap ref={modalRef} onClick={handleModalClick}>
-            <ModalContainer>
-              <p>
-                닫기와 확인 버튼 2개가 있고, 외부 영역을 눌러도 모달이 닫히지 않아요.
-              </p>
-              <ButtonContainer>
-                <StButton negative size='small' onClick={openModalHandler}>
-                  닫기
-                </StButton>
-                <StButton primary size='small'>
-                  확인
-                </StButton>
-              </ButtonContainer>
-            </ModalContainer>
-          </Wrap>
-        )}
-        {secShowModal && (
-          <Wrap>
-            <ModalContainer>
-              <StCloseButton>
-                <VscClose onClick={() => setSecShowModal(false)} />
-              </StCloseButton>
-              <p>닫기 버튼 1개가 있고,외부 영역을 누르면 모달이 닫혀요.</p>
-            </ModalContainer>
-          </Wrap>
-        )}
-      </>
-    );
-  }
+  const [showModal, setShowModal] = useState(false);
+  const [secShowModal, setSecShowModal] = useState(false);
+  const modalRef = useRef();
+
+  const openModalHandler = () => setShowModal(!showModal);
+  const openSecModalHandler = () => setSecShowModal(!secShowModal);
+
+  // 이벤트 버블링 막기(모달 내부 눌러도 외부영역 까지 전파되지 않게 해서)
+  const handleModalClick = e =>  e.stopPropagation();
+
+  return (
+    <>
+      <h1>Modal</h1>
+      <ButtonGroup>
+        <StButton primary size='small' onClick={openModalHandler}>
+          open modal
+        </StButton>
+        <StButton negative size='large' onClick={openSecModalHandler}>
+          open modal
+        </StButton>
+      </ButtonGroup>
+
+      {showModal && (
+        <Wrap>
+          <ModalContainer ref={modalRef}>
+            <p>닫기와 확인 버튼 2개가 있고, 외부 영역을 눌러도 모달이 닫히지 않아요.</p>
+            <ButtonContainer>
+              <StButton negative size='small' onClick={() => setShowModal(false)}>
+                닫기
+              </StButton>
+              <StButton primary size='small'>
+                확인
+              </StButton>
+            </ButtonContainer>
+          </ModalContainer>
+        </Wrap>
+      )}
+
+      {secShowModal && (
+        <Wrap>
+          <ModalContainer ref={modalRef} onClick={handleModalClick}>
+            <p>닫기 버튼 1개가 있고,외부 영역을 누르면 모달이 닫혀요.</p>
+            <CloseButton onClick={() => setSecShowModal(false)}>
+              <VscClose />
+            </CloseButton>
+          </ModalContainer>
+        </Wrap>
+      )}
+    </>
+  );
+}
 
 const Wrap = styled.div`
   position: fixed;
@@ -76,7 +71,6 @@ const ModalContainer = styled.div`
   position: relative;
   background-color: white;
   width: 300px;
-  height: 150px;
   padding: 20px;
   border-radius: 5px;
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
@@ -84,25 +78,23 @@ const ModalContainer = styled.div`
   flex-direction: column;
 `;
 
-const ButtonGrop = styled.div`
+const ButtonGroup = styled.div`
   display: flex;
   gap: 10px;
 `;
+
 const ButtonContainer = styled.div`
-  position: absolute;
   display: flex;
   gap: 5px;
   margin-top: 20px;
-  bottom: 10px;
-  right: 10px;
-  /* justify-content: flex-end; */
-  /* align-items: flex-end; */
+  justify-content: flex-end;
 `;
 
-const StCloseButton = styled.div`
-    display: flex;
-    justify-content: flex-end;
-`
-
+const CloseButton = styled.div`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  cursor: pointer;
+`;
 
 export default Modal;
