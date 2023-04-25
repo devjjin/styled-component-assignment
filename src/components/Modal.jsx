0c2d7bcf568a81e538fafc0d2614042e88bf547d
devjjin <sujink34@naver.com>
2023-04-25 18:32:18 +0900

    Feat: 모달 닫기아이콘 기능 추가

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,43 +1,65 @@
 import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
 import { StButton } from './Button';
+import { VscClose }  from 'react-icons/vsc';
 
 function Modal() {
-  const [showModal, setShowModal] = useState(false);
-  const modalRef = useRef();
-
-  const handleOpenModal = () => setShowModal(!showModal);
-
-  const handleModalClick = e => {
-    if (modalRef.current === e.target) {
-      return;
-    }
-  };
-
-  return (
-    <>
-      <h1>Modal</h1>
-      <StButton primary size='small' onClick={handleOpenModal}>
-        Open Modal
-      </StButton>
-      {showModal && (
-        <Wrap ref={modalRef} onClick={handleModalClick}>
-          <ModalContainer>
-            <p>닫기와 확인 버튼 2개가 있고, 외부 영역을 눌러도 모달이 닫히지 않아요.</p>
-            <ButtonContainer>
-              <StButton negative size='small' onClick={handleOpenModal}>
-                닫기
-              </StButton>
-              <StButton primary size='small'>
-                확인
-              </StButton>
-            </ButtonContainer>
-          </ModalContainer>
-        </Wrap>
-      )}
-    </>
-  );
-}
+    const [showModal, setShowModal] = useState(false);
+    const [secShowModal, setSecShowModal] = useState(false);
+    const modalRef = useRef();
+  
+    const openModalHandler = () => setShowModal(!showModal);
+    const openSecModalHandler = () => setSecShowModal(!secShowModal);
+  
+    const handleModalClick = e => {
+      if (modalRef.current === e.target) {
+          return;
+        }
+        setSecShowModal(false);
+    };
+  
+    return (
+      <>
+        <h1>Modal</h1>
+        <ButtonGrop>
+          <StButton primary size='small' onClick={openModalHandler}>
+            open modal
+          </StButton>
+          <StButton negative size='large' onClick={openSecModalHandler}>
+            open modal
+          </StButton>
+        </ButtonGrop>
+  
+        {showModal && (
+          <Wrap ref={modalRef} onClick={handleModalClick}>
+            <ModalContainer>
+              <p>
+                닫기와 확인 버튼 2개가 있고, 외부 영역을 눌러도 모달이 닫히지 않아요.
+              </p>
+              <ButtonContainer>
+                <StButton negative size='small' onClick={openModalHandler}>
+                  닫기
+                </StButton>
+                <StButton primary size='small'>
+                  확인
+                </StButton>
+              </ButtonContainer>
+            </ModalContainer>
+          </Wrap>
+        )}
+        {secShowModal && (
+          <Wrap>
+            <ModalContainer>
+              <StCloseButton>
+                <VscClose onClick={() => setSecShowModal(false)} />
+              </StCloseButton>
+              <p>닫기 버튼 1개가 있고,외부 영역을 누르면 모달이 닫혀요.</p>
+            </ModalContainer>
+          </Wrap>
+        )}
+      </>
+    );
+  }
 
 const Wrap = styled.div`
   position: fixed;
@@ -60,9 +82,12 @@ const ModalContainer = styled.div`
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
   display: flex;
   flex-direction: column;
-
 `;
 
+const ButtonGrop = styled.div`
+  display: flex;
+  gap: 10px;
+`;
 const ButtonContainer = styled.div`
   position: absolute;
   display: flex;
@@ -73,5 +98,11 @@ const ButtonContainer = styled.div`
   /* justify-content: flex-end; */
   /* align-items: flex-end; */
 `;
+
+const StCloseButton = styled.div`
+    display: flex;
+    justify-content: flex-end;
+`
+
 
 export default Modal;
